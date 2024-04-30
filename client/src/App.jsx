33478ba8f9e99697d272596dedf47bd5739ddc9a
Tendwa-T <tirus301@outlook.com>
2024-04-30 14:40:34 +0300

    chore: add client and server folders

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className='flex justify-center w-screen h-screen '>
+      <h1>
+        Hello Developer. Welcome to Pillars of Home Scaffold Website
+      </h1>
+    </div>
+  )
+}
+
+export default App
