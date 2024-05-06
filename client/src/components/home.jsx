0676fdddc627c72fb2a1def60c247a0218188749
Getonto3 <getontomoses18@gmail.com>
2024-05-06 18:27:19 +0300

    feat: nested BOX

--- client/src/components/home.jsx
+++ client/src/components/home.jsx
@@ -1,3 +1,23 @@
-export default function Home(){
-    return(<>Home page</>)
+import { Box } from "@mui/material";
+
+export default function Home() {
+  return (
+    <Box
+      sx={{
+        bgcolor: "#004e40",
+        height: "100vh",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <Box
+        sx={{
+          height: "100vh",
+          width: "80%",
+          margin: "0 auto",
+          backgroundColor: "rgba(255, 255, 255, 0.8)",
+        }}
+      ></Box>
+    </Box>
+  );
 }
