3656b04a6972a723fea803ce4b7218f21d7cfc5a
Getonto3 <getontomoses18@gmail.com>
2024-05-06 23:38:36 +0300

    feat: nested BOX for the home page

--- client/src/components/home.jsx
+++ client/src/components/home.jsx
@@ -5,19 +5,58 @@ export default function Home() {
     <Box
       sx={{
         bgcolor: "#004e40",
-        height: "100vh",
+        height: "98vh",
+        display: "flex",
         alignItems: "center",
         justifyContent: "center",
       }}
     >
       <Box
         sx={{
-          height: "100vh",
-          width: "80%",
-          margin: "0 auto",
+          height: "98vh",
+          width: "90%",
+          marginLeft: "auto", //align the box to the left
           backgroundColor: "rgba(255, 255, 255, 0.8)",
         }}
-      ></Box>
+      >
+        <Box
+          sx={{
+            height: "85vh",
+            width: "90%",
+            marginTop: "4em",
+            backgroundColor: "blue",
+            display: "flex",
+            
+          }}
+        >
+          <Box
+            sx={{
+             width:"55%",
+              backgroundColor: "primary.main",
+            }}
+          >
+           
+          </Box>
+          <Box
+            sx={{
+              flex: 1, //take up the remaining width
+              backgroundColor: "red",
+            }}
+          >
+           
+          </Box>
+        </Box>
+        <Box
+          sx={{
+            height: "4em",
+            width: "90%",
+           
+            backgroundColor: "green ",
+            display: "flex",
+            
+          }}
+        ></Box>
+      </Box>
     </Box>
   );
 }
