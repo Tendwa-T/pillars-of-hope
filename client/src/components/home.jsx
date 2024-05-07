2a0558d930d08d6a087c3ba89d677b8341aff555
Getonto3 <getontomoses18@gmail.com>
2024-05-08 02:20:12 +0300

    chore: update home page & rename home component

--- client/src/components/home.jsx
+++ client/src/components/home.jsx
@@ -1,62 +1,62 @@
 import { Box } from "@mui/material";
 
+
 export default function Home() {
   return (
-    <Box
-      sx={{
-        bgcolor: "#004e40",
-        height: "98vh",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-      }}
-    >
+    <>
       <Box
         sx={{
+          bgcolor: "#004e40",
           height: "98vh",
-          width: "90%",
-          marginLeft: "auto", //align the box to the left
-          backgroundColor: "rgba(255, 255, 255, 0.8)",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
         }}
       >
         <Box
           sx={{
-            height: "85vh",
+            height: "98vh",
             width: "90%",
-            marginTop: "4em",
-            backgroundColor: "blue",
-            display: "flex",
-            
+            marginLeft: "auto", //align the box to the left
+            backgroundColor: "rgba(255, 255, 255, 0.8)",
+           
           }}
         >
           <Box
             sx={{
-             width:"55%",
-              backgroundColor: "primary.main",
+              height: "85vh",
+              width: "90%",
+              marginTop: "4em",
+              backgroundColor: "blue",
+              display: "flex",
+              justifyContent: "space-between",
+            
             }}
           >
-           
+            <Box
+              sx={{
+                width: "55%",
+                backgroundColor: "primary.main",
+              }}
+            ></Box>
+            <Box
+              sx={{
+                flex: 1, //take up the remaining width
+                backgroundColor: "red",
+              }}
+            ></Box>
           </Box>
           <Box
             sx={{
-              flex: 1, //take up the remaining width
-              backgroundColor: "red",
+              height: "4em",
+              width: "90%",
+
+              backgroundColor: "green ",
+              display: "flex",
             }}
-          >
-           
-          </Box>
+          ></Box>
         </Box>
-        <Box
-          sx={{
-            height: "4em",
-            width: "90%",
-           
-            backgroundColor: "green ",
-            display: "flex",
-            
-          }}
-        ></Box>
       </Box>
-    </Box>
+    </>
   );
 }
--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -2,11 +2,13 @@ import DrawerComponent from "../components/admin/Drawer";
 import HomeIcon from "@mui/icons-material/Home";
 import DashboardIcon from "@mui/icons-material/Dashboard";
 import SettingsIcon from "@mui/icons-material/Settings";
-import Home from "../components/home";
+import Home from "../components/Home";
+import DrawerAppBar from "../components/Navbar";
 
 export default function HomePage() {
   return (
     <>
+      <DrawerAppBar />
       <Home />
     </>
   );
