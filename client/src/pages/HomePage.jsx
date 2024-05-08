70f985a9ba5c25c7ef3f07ece30f411e00af3922
Maryleen Menecha <menechamaryleen@gmail.com>
2024-05-08 13:01:55 +0300

    Added footer here

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,12 +1,17 @@
 
 import AboutPage from "../components/AboutSection";
 import AdminEventsSection from "../components/admin/EventsSection";
+import Support from "../components/Support";
+import Footer from "../components/footer";
 
 export default function HomePage() {
     return (
         <>
             <AdminEventsSection />
             <AboutPage />
+            <Support />
+            <Footer/>
+
         </>
     );
 }
