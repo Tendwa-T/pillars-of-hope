cdf8b86f93b5bd39cab7da6df4fcf2c95a8ffa70
Abel-mwirigi <mwirigiabel223359@daystar.ac.ke>
2024-05-09 20:39:43 +0300

    corrected on importation

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -2,7 +2,7 @@ import DrawerComponent from "../components/admin/Drawer";
 import HomeIcon from "@mui/icons-material/Home";
 import DashboardIcon from "@mui/icons-material/Dashboard";
 import SettingsIcon from "@mui/icons-material/Settings";
-import Home from "../components/Home";
+import Home from "../components/home";
 import DrawerAppBar from "../components/Navbar";
 import AboutPage from "../components/AboutSection";
 import AdminEventsSection from "../components/admin/EventsSection";
