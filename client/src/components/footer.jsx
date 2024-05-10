243fa365a2ac9a558baf1f28b9cb79dd300e9c10
Maryleen Menecha <menechamaryleen@gmail.com>
2024-05-10 18:40:38 +0300

    Feat: Added responsive footer

--- client/src/components/footer.jsx
+++ client/src/components/footer.jsx
@@ -1,4 +1,4 @@
-import { Button, Stack, Container } from "@mui/material";
+import { Button,Box, Typography, Stack, Container} from "@mui/material";
 import IconButton from "@mui/material/IconButton";
 import PhoneIcon from "@mui/icons-material/Phone";
 import LocationOnIcon from "@mui/icons-material/LocationOn";
@@ -9,34 +9,54 @@ export default function Footer() {
   return (
     <Container
       maxWidth="screen"
-      xs={6}
+      xs={12}
       sm={3}
-      
       sx={{
-        display: "flex",
         gap: 4,
         padding: 4,
         bgcolor: "#004e40",
       }}
     >
-      <Stack direction="row" spacing={2}>
+      <Stack
+        direction={{ xs: "column", sm: "row" }}
+        spacing={{ xs: 1, sm: 2, md: 4 }}
+      >
         <Button
-          sx={{ bgcolor: "#fed75e" }}
+          sx={{
+            bgcolor: "#fed75e",
+            "&:hover": {
+              bgcolor: "#004e40",
+            },
+          }}
           variant="contained"
+          size="medium"
           startIcon={<PhoneIcon />}
         >
           Phone <br /> 123-456-7890
         </Button>
         <Button
-          sx={{ bgcolor: "#fed75e" }}
+          sx={{
+            bgcolor: "#fed75e",
+            "&:hover": {
+              bgcolor: "#004e40",
+            },
+          }}
           variant="contained"
+          size="medium"
           startIcon={<LocationOnIcon />}
         >
           Athi River, Mavoko <br /> Machakos County
         </Button>
         <Button
-          sx={{ bgcolor: "#fed75e", roundedRadius: 2 }}
+          sx={{
+            bgcolor: "#fed75e",
+            roundedRadius: 2,
+            "&:hover": {
+              bgcolor: "#004e40",
+            },
+          }}
           variant="contained"
+          size="large"
         >
           Mpesa paybill: 874 580 <br />
           Cooperative bank branch: Athi River <br />
@@ -44,9 +64,18 @@ export default function Footer() {
           Account Name: Pillars Of Hope Childrens Home
         </Button>
         <IconButton size="medium">
-          <FacebookIcon sx={{bgcolor:"#fff"}}/>
+          <FacebookIcon sx={{ bgcolor: "#fff" }} />
         </IconButton>
+        <Box sx={{ padding: 2, maxWidth: "screen", marginTop: 4 }}>
+          <Typography variant="h6" sx={{marginTop:10}}>
+            All rights reserved by Pillars of Hope Childrens Home
+          </Typography>
+        </Box>
       </Stack>
+      <Box sx={{display:"flex", gap:12}}>
+        
+        
+      </Box>
     </Container>
   );
 }
